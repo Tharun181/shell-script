b81ab6e77141fe752bb862d33cd21deaf5f623db
Tharun181 <tharunch11.com>
2026-07-03 00:12:41 +0530

    functions

---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo "$2..failure"
+        exit1
+    else
+       echo "$2..success"
+    fi
+}
+
+if[ $USERID -ne 0]
+then
+    echo "please run this script with root access."
+    exit1
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "installing MYSQL"
+
+dnf install git -y
+VALIDATE $? "installing git"
