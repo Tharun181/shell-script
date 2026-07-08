1a1f8016a2252d7ba9cfacf44d9cc9850338a037
Tharun181 <tharunch11.com>
2026-07-08 20:39:41 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+echo "Script started executing at: $TIMESTAMP"
 
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo "$2...FAILURE"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
@@ -20,8 +28,9 @@ else
     echo "You are super user"
 fi
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
 VALIDATE $? "Installing MYSQL"
 
-dnf install git -y
+dnf install git -y &>>LOGFILE
 VALIDATE $? "Installing GIT"
+
